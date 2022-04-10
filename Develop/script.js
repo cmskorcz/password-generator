e64e575e658560003c8df9af1acbe9792253f828
cmskorcz <chaseskorey@icloud.com>
2022-04-10 14:48:05 -0400

    password generates and appears in document

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment code here
 
+// Set up array of alphabet
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+// Initialize empty newPassword array and characterLength
+let newPassword = [];
+let characterLength = ""
+
 let passwordParam = {
   length: 8,
   includeLower: false,
@@ -8,16 +15,39 @@ let passwordParam = {
   includeSpecial: false
 };
 
+// Generate Password
 function generatePassword() {
-  let characterLength = prompt("Enter how many characters you would like your password to be. Length must be between 8 and 128 characters.")
 
+  // Reset newPassword array and characterLength
+  newPassword = [];
+  characterLength = ""
+
+  // Specify character length of password
+  characterLength = prompt("Enter how many characters you would like your password to be. Length must be between 8 and 128 characters.")
+
+  // Check if prompt value is a number between 8 and 128
   if (parseInt(characterLength) >= 8 && parseInt(characterLength) <= 128) {
-    console.log(characterLength);
+    
+    // Map over alphabet array using characterLength as limit
+    for (let i = 0; i < characterLength; i++) {
+
+      // Pushes random letter from alphabet array to newPassword array with each loop
+      let letter = Math.floor(Math.random() * 27);
+      newPassword.push(alphabet[letter]);
+    };
+
   } else {
+    
+    // If valid num not entered, alert user and restart function
     alert("Please enter a valid length")
     generatePassword();
-  }
+  };
+
+  // newPassword is converted to string and commas are removed, then value returned
+  return newPassword.toString().replace(/,/g, "");
 }
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
