b42628b275e2282f161a3e3f5aabf145235c2460
cmskorcz <chaseskorey@icloud.com>
2022-04-10 21:46:46 -0400

    added number array and map

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,8 @@
 // Assignment code here
 
-// Set up array of alphabet
+// Set up array of alphabet and numbers
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // Initialize empty newPassword array and passwordParam
 let newPassword = [];
@@ -31,6 +32,25 @@ function mixedCase(pass) {
   return mixedArray;
 }
 
+  // Map over alphabet array using length as limit
+function mapAlphabet(array) {
+  for (let i = 0; i < passwordParam.length; i++) {
+
+    // Pushes random letter from alphabet array to newPassword array with each loop
+    let letter = Math.floor(Math.random() * 26);
+    array.push(alphabet[letter]);
+  };
+  return array;
+}
+
+function mapNumber(array) {
+  for (let i = 0; i < passwordParam.length; i++) {
+    let num = Math.floor(Math.random() * 10);
+    array.push(numbers[num]);
+  }
+  return array;
+}
+
 // Generate Password
 function generatePassword() {
 
@@ -39,6 +59,7 @@ function generatePassword() {
   passwordParam.length = "";
   passwordParam.includeLower = true;
   passwordParam.includeUpper = false;
+  passwordParam.includeNum = false;
 
   // Specify character length of password
   passwordParam.length = prompt("Enter how many characters you would like your password to be. Length must be between 8 and 128 characters.")
@@ -52,38 +73,41 @@ function generatePassword() {
     // Confirm Upper Case
     passwordParam.includeUpper = confirm("Would you like your password to contain uppercase letters?");
 
-    // Map over alphabet array using length as limit
-    for (let i = 0; i < passwordParam.length; i++) {
-
-      // Pushes random letter from alphabet array to newPassword array with each loop
-      let letter = Math.floor(Math.random() * 26);
-      newPassword.push(alphabet[letter]);
-    };
+    // Confirm Numbers
+    passwordParam.includeNum = confirm("Would you like your password to contain numbers?");
 
   } else {
-    
     // If valid num not entered, alert user and restart function
     alert("Please enter a valid length")
     generatePassword();
+
   };
 
   // newPassword is converted to string and commas are removed, string is then adjusted for case
 
-  // Both Upper and Lower Case
+  // Upper and Lower Case
   if (passwordParam.includeLower && passwordParam.includeUpper) {
-    newPassword = stringifyPassword(mixedCase(newPassword));
+    newPassword = stringifyPassword(mixedCase(mapAlphabet(newPassword)));
     return newPassword;
 
   // Lower Case Only
   } else if (passwordParam.includeLower && !passwordParam.includeUpper) {
-    newPassword = stringifyPassword(newPassword);
+    newPassword = stringifyPassword(mapAlphabet(newPassword));
     return newPassword.toLowerCase();
 
   // Upper Case Only
   } else if (!passwordParam.includeLower && passwordParam.includeUpper) {
-    newPassword = stringifyPassword(newPassword);
+    newPassword = stringifyPassword(mapAlphabet(newPassword));
     return newPassword.toUpperCase();
+
+  // Numbers Only
+  } else if (!passwordParam.includeLower && !passwordParam.includeUpper && passwordParam.includeNum) {
+    newPassword = stringifyPassword(mapNumber(newPassword));
+    return newPassword;
   }
+  
+
+  
 }
 
 
