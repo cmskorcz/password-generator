47595e131eee7dedc0ac5e974389e0117387c7b0
cmskorcz <chaseskorey@icloud.com>
2022-04-10 22:36:00 -0400

    added symbols constant

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@
 // Set up array of alphabet and numbers
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const symbols = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
 
 // Initialize empty newPassword array and passwordParam
 let newPassword = [];
